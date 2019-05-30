4ed266c28f3f89c99f59ec87fb870c6b887254d1
Ricardo Pereira <contato@ricardopdias.com.br>
2019-05-30 14:17:18 -0300

    Adicionadas versões extras do PHP

--- docker-reload.sh
+++ docker-reload.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sudo docker-compose down
-sudo docker system prune -a --volumes
+# sudo docker system prune -a --volumes
 # sudo docker image rm aiguilles-dealers_boot
-# sudo docker image rm aiguilles-dealers_php
+# sudo docker container prune
+sudo docker image rm php
+sudo docker image rm docker-php-project_php 
 sudo docker-compose up
