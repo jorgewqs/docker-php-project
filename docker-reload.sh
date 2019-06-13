e955bb93c4eaa762a0307d1838193e99d5e3a62a
Ricardo Pereira <contato@ricardopdias.com.br>
2019-06-13 10:54:04 -0300

    Remoção do script de testes docker-reload.sh

--- docker-reload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo docker-compose down
-# sudo docker system prune -a --volumes
-# sudo docker image rm aiguilles-dealers_boot
-# sudo docker container prune
-sudo docker image rm php
-sudo docker image rm docker-php-project_php 
-sudo docker-compose up
